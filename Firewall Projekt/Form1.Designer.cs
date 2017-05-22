99f2008c5c0e44c698ae98dbfe9fdd2f8ad5ee4e
Artur B <arturzubert@web.de>
2017-05-22 14:42:06 +0200

    Add files via upload

---
+++ Firewall Projekt/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Firewall_Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxStream = new System.Windows.Forms.ListBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.netzwerkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.interneUndExterneIPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zeigeInternetKommunikationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spieleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pingPongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tABSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schnellesBackupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailBackupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cS16ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxStream
+            // 
+            this.listBoxStream.FormattingEnabled = true;
+            this.listBoxStream.Location = new System.Drawing.Point(12, 43);
+            this.listBoxStream.Name = "listBoxStream";
+            this.listBoxStream.Size = new System.Drawing.Size(380, 316);
+            this.listBoxStream.TabIndex = 6;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(311, 365);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(81, 22);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "Listbox Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.netzwerkToolStripMenuItem,
+            this.spieleToolStripMenuItem,
+            this.backupToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(404, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // netzwerkToolStripMenuItem
+            // 
+            this.netzwerkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.interneUndExterneIPToolStripMenuItem,
+            this.zeigeInternetKommunikationToolStripMenuItem});
+            this.netzwerkToolStripMenuItem.Name = "netzwerkToolStripMenuItem";
+            this.netzwerkToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.netzwerkToolStripMenuItem.Text = "Netzwerk";
+            // 
+            // interneUndExterneIPToolStripMenuItem
+            // 
+            this.interneUndExterneIPToolStripMenuItem.Name = "interneUndExterneIPToolStripMenuItem";
+            this.interneUndExterneIPToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
+            this.interneUndExterneIPToolStripMenuItem.Text = "Interne und Externe IP";
+            this.interneUndExterneIPToolStripMenuItem.Click += new System.EventHandler(this.interneUndExterneIPToolStripMenuItem_Click);
+            // 
+            // zeigeInternetKommunikationToolStripMenuItem
+            // 
+            this.zeigeInternetKommunikationToolStripMenuItem.Name = "zeigeInternetKommunikationToolStripMenuItem";
+            this.zeigeInternetKommunikationToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
+            this.zeigeInternetKommunikationToolStripMenuItem.Text = "Zeige Internet Kommunikation";
+            this.zeigeInternetKommunikationToolStripMenuItem.Click += new System.EventHandler(this.zeigeInternetKommunikationToolStripMenuItem_Click);
+            // 
+            // spieleToolStripMenuItem
+            // 
+            this.spieleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pingPongToolStripMenuItem,
+            this.tABSToolStripMenuItem,
+            this.cS16ToolStripMenuItem});
+            this.spieleToolStripMenuItem.Name = "spieleToolStripMenuItem";
+            this.spieleToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.spieleToolStripMenuItem.Text = "Spiele";
+            // 
+            // pingPongToolStripMenuItem
+            // 
+            this.pingPongToolStripMenuItem.Name = "pingPongToolStripMenuItem";
+            this.pingPongToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pingPongToolStripMenuItem.Text = "Ping Pong";
+            this.pingPongToolStripMenuItem.Click += new System.EventHandler(this.pingPongToolStripMenuItem_Click);
+            // 
+            // tABSToolStripMenuItem
+            // 
+            this.tABSToolStripMenuItem.Name = "tABSToolStripMenuItem";
+            this.tABSToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tABSToolStripMenuItem.Text = "TABS";
+            this.tABSToolStripMenuItem.Click += new System.EventHandler(this.tABSToolStripMenuItem_Click);
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schnellesBackupToolStripMenuItem,
+            this.detailBackupToolStripMenuItem});
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.backupToolStripMenuItem.Text = "Backup";
+            // 
+            // schnellesBackupToolStripMenuItem
+            // 
+            this.schnellesBackupToolStripMenuItem.Name = "schnellesBackupToolStripMenuItem";
+            this.schnellesBackupToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.schnellesBackupToolStripMenuItem.Text = "Schnelles USB Backup";
+            this.schnellesBackupToolStripMenuItem.Click += new System.EventHandler(this.schnellesBackupToolStripMenuItem_Click);
+            // 
+            // detailBackupToolStripMenuItem
+            // 
+            this.detailBackupToolStripMenuItem.Name = "detailBackupToolStripMenuItem";
+            this.detailBackupToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.detailBackupToolStripMenuItem.Text = "Detail Backup";
+            this.detailBackupToolStripMenuItem.Click += new System.EventHandler(this.detailBackupToolStripMenuItem_Click);
+            // 
+            // cS16ToolStripMenuItem
+            // 
+            this.cS16ToolStripMenuItem.Name = "cS16ToolStripMenuItem";
+            this.cS16ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cS16ToolStripMenuItem.Text = "CS 1.6";
+            this.cS16ToolStripMenuItem.Click += new System.EventHandler(this.cS16ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(404, 389);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.listBoxStream);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Multifunktionstool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxStream;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem netzwerkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem interneUndExterneIPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zeigeInternetKommunikationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spieleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pingPongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tABSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schnellesBackupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detailBackupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cS16ToolStripMenuItem;
+    }
+}
+
---
+++ Firewall Projekt/Form1.cs
@@ -0,0 +1,224 @@
+﻿#region Using's
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.IO;
+using System.Data.OleDb;
+#endregion
+
+namespace Firewall_Projekt
+{
+    public partial class Form1 : Form
+    {
+        Process winpcap;
+        Process Wireshark;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+
+            }
+
+            else
+            {
+                MessageBox.Show("Sie haben kein 64Bit Betriebssystem. Bitte Downloaden Sie Wireshark 32Bit um die Internet Kommunikation anzeigen lassen zu können.\nDer Download Link wurde in ihren Zwischenspeicher kopiert.", "Achtung");
+                System.Windows.Forms.Clipboard.SetDataObject("https://www.wireshark.org/#download", true);
+            }
+        }
+
+        private void interneUndExterneIPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBoxStream.Items.Add("Lade IP-Adresse... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            string sHostName = Dns.GetHostName();
+            var vHost = Dns.GetHostEntry(Dns.GetHostName());
+            listBoxStream.Items.Add("Gebe IP-Adresse(n) aus. " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+
+            foreach (var ip in vHost.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    listBoxStream.Items.Add("IP Adresse: " + ip + "     Host Name/PC Name: " + sHostName);
+                }
+            }
+
+            listBoxStream.Items.Add("Suche Öffentliche IP. Bitte warten... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            Thread.Sleep(500);
+            GetMyInternetIpAddress();
+        }
+
+        private void zeigeInternetKommunikationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBoxStream.Items.Add("Starte Winpcap.exe... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            Directory.SetCurrentDirectory(@"..\");
+            String sDir = Directory.GetCurrentDirectory();
+            winpcap = Process.Start("debug\\WinPcap.exe");
+
+            try
+            {
+                do
+                {
+                    Thread.Sleep(3000);
+                    listBoxStream.Items.Add("Warte auf Beenden von WinPcap.exe... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                } while (!winpcap.HasExited);
+
+                listBoxStream.Items.Add("WinPcap.exe wurde beendet, starte Wireshark.exe " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                Wireshark = Process.Start("Wireshark\\Wireshark.exe");
+
+                do
+                {
+
+                } while (!Wireshark.HasExited);
+
+                listBoxStream.Items.Add("Wireshark.exe wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                listBoxStream.Items.Add("");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listBoxStream.Items.Add("Fehler aufgetreten. Wurde nicht ordnungsgemäß beendet. " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                listBoxStream.Items.Add("");
+            }
+        }
+
+        private void pingPongToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process Pong;
+            listBoxStream.Items.Add("Starte PingPong by Artur... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            Pong = Process.Start("PingPongbyArtur.exe");
+
+            do
+            {
+
+            } while (!Pong.HasExited);
+
+            listBoxStream.Items.Add("PingPong by Artur wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+        }
+
+        private void tABSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Directory.SetCurrentDirectory(@"..\");
+            Process TABS;
+            listBoxStream.Items.Add("Starte T.A.B.S ... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            TABS = Process.Start("TotallyAccurateBattleSimulator.exe");
+
+            do
+            {
+
+            } while (!TABS.HasExited);
+
+            listBoxStream.Items.Add("T.A.B.S wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            listBoxStream.Items.Add("");
+        }
+
+        private void schnellesBackupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process AutoUSB;
+            listBoxStream.Items.Add("Starte Schnell USB Backup ... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            AutoUSB = Process.Start("AutoBackup.exe");
+
+            do
+            {
+
+            } while (!AutoUSB.HasExited);
+
+            listBoxStream.Items.Add("Schnell USB Backup wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            listBoxStream.Items.Add("");
+        }
+
+        private void detailBackupToolStripMenuItem_Click(object sender, EventArgs e)
+        {     
+            Process USB;
+            listBoxStream.Items.Add("Starte Backup Programm Made by Artur und Marvin... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            USB = Process.Start("USB.exe");
+
+            do
+            {
+
+            } while (!USB.HasExited);
+
+            listBoxStream.Items.Add("Backup Programm Made by Artur und Marvin wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            listBoxStream.Items.Add("");
+        }
+
+        private void cS16ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process CS;
+            listBoxStream.Items.Add("Starte Counter Strike 1.6 ... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            CS = Process.Start("CounterStrike.exe");
+
+            do
+            {
+
+            } while (!CS.HasExited);
+
+            listBoxStream.Items.Add("Counter Strike 1.6 wurde beendet." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+            listBoxStream.Items.Add("");
+        }
+
+        #region IP Adresse ausserhalb bekommen
+        public IPAddress GetMyInternetIpAddress()
+        {
+            WebRequest hwr = HttpWebRequest.Create(new Uri("http://checkip.dyndns.org"));
+            WebResponse wr;
+            Stream stream;
+            StreamReader streamReader;
+            string htmlResult;
+            string[] htmlSplit;
+            string IP;
+
+            try
+            {
+                wr = hwr.GetResponse();
+                stream = wr.GetResponseStream();
+                streamReader = new StreamReader(stream, Encoding.UTF8);
+                htmlResult = streamReader.ReadToEnd();
+                htmlSplit = htmlResult.Split(new string[] { ":", "<" }, StringSplitOptions.RemoveEmptyEntries);
+                IP = htmlSplit[6].Trim();
+                stream.Close();
+                wr.Close();
+                listBoxStream.Items.Add("IP gefunden... " + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                listBoxStream.Items.Add("Öffentliche IP: " + IP);
+                listBoxStream.Items.Add("");
+                return IPAddress.Parse(IP);
+            }
+            catch (Exception ex)
+            {
+                if (MessageBox.Show(ex.Message, "Fehler!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Exclamation) == DialogResult.Retry)
+                {
+                    listBoxStream.Items.Clear();
+                    listBoxStream.Items.Add("Versuche erneute Verbindung..." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                    GetMyInternetIpAddress();
+                }
+                else
+                {
+                    listBoxStream.Items.Add("Fehler aufgetreten..." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                    listBoxStream.Items.Add("Operation abgebrochen..." + Convert.ToString(DateTime.Now.ToLongTimeString()));
+                    listBoxStream.Items.Add("");
+                }
+                return IPAddress.Parse(IP = "111.111.111.111");
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            listBoxStream.Items.Clear();
+        }
+        #endregion
+
+    }
+}
